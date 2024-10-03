bbce406f5c10b7d215c56af82af056d21769c333
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-02 21:38:29 -0500

    Se creó la clase Episodio y se agregó sus atributos y se generó los getter y setter para los atributos de dicha clase

---
+++ src/com/aluracursos/screenmatch/modelos/Episodio.java
@@ -0,0 +1,31 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Episodio {
+    private int numero;
+    private String nombre;
+    private Serie serie;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+}
