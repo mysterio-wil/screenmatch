5774947b1688a3a70b746b36d3d4d760aa15212f
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 18:15:52 -0500

    Creacion de una instancia de FiltroClasificacion y aplocacion de filta a 'miPelicula' y la creacion de una instaciona de Episodio y la configuracion de sus atributos.

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.calculos.FiltroClasificacion;
+import com.aluracursos.screenmatch.modelos.Episodio;
 import com.aluracursos.screenmatch.modelos.Pelicula;
 import com.aluracursos.screenmatch.modelos.Serie;
 
@@ -36,5 +38,17 @@ public class Principal {
         calculadora.incluye(casaDragon);
         calculadora.incluye(otraPelicula);
         System.out.println("Tiempo necesario para ver tus títulos favoritos estas vacaciones " + calculadora.getTiempoTotal() + " minutos");
+
+        FiltroClasificacion filtroClasificacion = new FiltroClasificacion();
+        filtroClasificacion.filtra(miPelicula);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setNombre("La casa Targaryen");
+        episodio.setSerie(casaDragon);
+        episodio.setTotalVisualizaciones(300);
+
+        filtroClasificacion.filtra(episodio);
+
     }
 }
