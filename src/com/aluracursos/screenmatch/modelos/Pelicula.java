a989ce1c1494b1cc28465aa9ab89d9902ef6fe59
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 16:52:24 -0500

    Se importó la interface Clasificacion y se uso 'implements' en la class Pelicula para implementar 'Clasificacion' y por ultimo se agregó el método getClasificacon() con el indicador de anotacion @Override.

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula extends Titulo {
+import com.aluracursos.screenmatch.calculos.Clasificacion;
+
+public class Pelicula extends Titulo implements Clasificacion {
     private String director;
 
     public String getDirector() {
@@ -9,5 +11,10 @@ public class Pelicula extends Titulo {
 
     public void setDirector(String director) {
         this.director = director;
+    }
+
+    @Override
+    public int getClasificacion() {
+        return (int) (calculaMedia() / 2);
     }
 }
