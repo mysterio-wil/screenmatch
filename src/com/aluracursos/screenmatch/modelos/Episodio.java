1b0a2b7d242a05367f053d5cd030488ce93a3630
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 17:12:08 -0500

    Adicionamos 'implements' Clasificacion en la clase Episodio y añadimos el atributo totalVisualizaciones y adicionamos el método getClasificacion () con el indicacador de anotacion @Override e implementamos if-else.

--- src/com/aluracursos/screenmatch/modelos/Episodio.java
+++ src/com/aluracursos/screenmatch/modelos/Episodio.java
@@ -1,9 +1,12 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Episodio {
+import com.aluracursos.screenmatch.calculos.Clasificacion;
+
+public class Episodio implements Clasificacion {
     private int numero;
     private String nombre;
     private Serie serie;
+    private int totalVisualizaciones;
 
     public int getNumero() {
         return numero;
@@ -27,5 +30,14 @@ public class Episodio {
 
     public void setSerie(Serie serie) {
         this.serie = serie;
+    }
+
+    @Override
+    public int getClasificacion() {
+        if (totalVisualizaciones > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
     }
 }
